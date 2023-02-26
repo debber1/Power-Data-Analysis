5d947e7c439854eadeb96ca4ce82cd00e4386fa6
debber1 <robbe.decapmaker@hotmail.be>
2023-02-26 15:34:03 +0100

    Adding power meter data

--- db.py
+++ db.py
@@ -35,6 +35,35 @@ def solarTimePeriod(host, port, authToken, myOrg, bucket, startDate, stopDate):
   |> range(start: '+ startDate.strftime('%Y-%m-%dT%H:%M:%SZ')+', stop: '+ stopDate.strftime('%Y-%m-%dT%H:%M:%SZ') +')\
   |> filter(fn: (r) => r["_measurement"] == "W")\
   |> filter(fn: (r) => r["_field"] == "value")\
+  |> filter(fn: (r) => r["entity_id"] == "pv_power")\
+  |> aggregateWindow(every: 1s, fn: last, createEmpty: false)\
+  |> yield(name: "last")'
+    result = queryApi.query(org=myOrg, query=query)
+    results = []
+    for table in result:
+        for record in table.records:
+            results.append([utcToLocal(record.get_time()), record.get_value()])
+    return(results)
+
+def powerMeterTimePeriod(host, port, authToken, myOrg, bucket, startDate, stopDate):
+    """
+    This function takes the relevant data for an influxdb and connects to it. Then, it fetches powerMeter data from startdate to stopDate
+
+    :param host: String: IP of the influxdb
+    :param port: String: Port of the influxdb
+    :param authToken: String: Token used for identification
+    :param myOrg: String: Organisation ID
+    :param startDate: DateTime: The date and time where the data starts
+    :param endDate: DateTime: The date and time where the data ends
+    :return: List of List: A list with lists containing data points: [[time, data], [time, data]]
+    """
+    client = InfluxDBClient(url=host+":"+port, token= authToken, org=myOrg)
+    queryApi = client.query_api()
+    query = 'from(bucket: "'+bucket+'")\
+  |> range(start: '+ startDate.strftime('%Y-%m-%dT%H:%M:%SZ')+', stop: '+ stopDate.strftime('%Y-%m-%dT%H:%M:%SZ') +')\
+  |> filter(fn: (r) => r["_measurement"] == "W")\
+  |> filter(fn: (r) => r["_field"] == "value")\
+  |> filter(fn: (r) => r["entity_id"] == "p1_meter_5c2faf05011e_active_power")\
   |> aggregateWindow(every: 1s, fn: last, createEmpty: false)\
   |> yield(name: "last")'
     result = queryApi.query(org=myOrg, query=query)
--- load.py
+++ load.py
@@ -24,3 +24,24 @@ def loadDataDayScale(startDay, numberOfDays):
         days.append(period)
     return days
  
+def loadDataPowerMeterDayScale(startDay, numberOfDays):
+    """
+    This function loads the relevant data for a timeperiod and devides it in daily segments
+
+    :param startDay: DateTime: the day where datacollection should start
+    :param numberOfDays: Int: The number of days we want to pull the data from
+    :return: List of timePeriod: List of class with relevant data for a segment of a day
+    """
+    days = []
+    for day in range(0,numberOfDays):
+        print("LOAD: loading day "+str(day + 1)+" of "+str(numberOfDays))
+        startTime = datetime.now()
+        data = powerMeterTimePeriod(getValue("host"),getValue("port"),getValue("token"),getValue("org"),getValue("bucket"), startDay + timedelta(days=day), startDay + timedelta(days=day+1))
+        endTime = datetime.now()
+        print("LOAD: It took "+ str((endTime-startTime).total_seconds())+" seconds to load the data")
+        startTime = datetime.now()
+        period = timePeriodData((startDay+timedelta(days=day)).strftime("%d/%m/%Y"), startDay + timedelta(days=day), startDay + timedelta(days=day+1), rebaseTimeSeriesSolar(data))
+        endTime = datetime.now()
+        print("LOAD: It took "+ str((endTime-startTime).total_seconds())+" seconds to convert the data")
+        days.append(period)
+    return days
